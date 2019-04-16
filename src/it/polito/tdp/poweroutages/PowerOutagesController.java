3d0e6a3a3a90c2a70523494e8bea7537215b8e24
ruben <ruben@172.22.57.12>
2019-04-16 16:00:44 +0200

    Creata interfaccia grafica ed implementato pattern MVC

--- src/it/polito/tdp/poweroutages/PowerOutagesController.java
+++ src/it/polito/tdp/poweroutages/PowerOutagesController.java
@@ -1,6 +1,58 @@
 package it.polito.tdp.poweroutages;
 
-public class PowerOutagesController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import it.polito.tdp.poweroutages.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class PowerOutagesController {
 	
+	private Model model = new Model();
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ChoiceBox<?> selectNERC;
+
+    @FXML
+    private TextField txtYears;
+
+    @FXML
+    private TextField txtHours;
+
+    @FXML
+    private Button btnAnalizza;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doAnalisi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert selectNERC != null : "fx:id=\"selectNERC\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+        assert txtYears != null : "fx:id=\"txtYears\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+        assert txtHours != null : "fx:id=\"txtHours\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+        assert btnAnalizza != null : "fx:id=\"btnAnalizza\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+
+    }
+
+    
+    public void setModel(Model model) {
+    	this.model = model;
+    }
 }
